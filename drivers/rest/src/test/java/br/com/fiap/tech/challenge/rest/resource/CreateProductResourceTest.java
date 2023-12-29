f4e3eb2070ff6833a114cd816754f02df8f49d24
Eberson Oliveira <eberson.olivera@gmail.com>
2023-12-28 21:21:34 -0300

    feat: added test for create combo product

--- drivers/rest/src/test/java/br/com/fiap/tech/challenge/rest/resource/CreateProductResourceTest.java
+++ drivers/rest/src/test/java/br/com/fiap/tech/challenge/rest/resource/CreateProductResourceTest.java
@@ -5,20 +5,19 @@ import br.com.fiap.tech.challenge.application.dto.CreateProductDTO;
 import br.com.fiap.tech.challenge.rest.mapping.CreateProductMapper;
 import br.com.fiap.tech.challenge.rest.mapping.ProductResponseMapper;
 import br.com.fiap.tech.challenge.rest.util.Fixture;
-import br.com.fiap.tech.challenge.rest.util.ProductDTOFixture;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-import static br.com.fiap.tech.challenge.rest.util.CreateProductRequestFixture.singleProductModel;
+import static br.com.fiap.tech.challenge.rest.util.CreateProductRequestFixture.comboProductRequestModel;
+import static br.com.fiap.tech.challenge.rest.util.CreateProductRequestFixture.singleProductRequestModel;
+import static br.com.fiap.tech.challenge.rest.util.ProductDTOFixture.comboProductModel;
 import static br.com.fiap.tech.challenge.rest.util.ProductDTOFixture.sandwichModel;
 import static br.com.fiap.tech.challenge.rest.util.Products.asJsonString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -27,10 +26,9 @@ class CreateProductResourceTest extends ResourceBaseTestSuite<CreateProductResou
     @Mock
     private CreateProductController productController;
 
-
     @Test
     void testCreatingSingleProduct() throws Exception {
-        var request = Fixture.create(singleProductModel());
+        var request = Fixture.create(singleProductRequestModel());
         var response = Fixture.create(sandwichModel());
 
         when(productController.create(any(CreateProductDTO.class))).thenReturn(response);
@@ -45,6 +43,22 @@ class CreateProductResourceTest extends ResourceBaseTestSuite<CreateProductResou
         verify(productController).create(any(CreateProductDTO.class));
     }
 
+    @Test
+    void testCreatingComboProduct() throws Exception {
+        var request = Fixture.create(comboProductRequestModel());
+        var response = Fixture.create(comboProductModel());
+
+        when(productController.create(any(CreateProductDTO.class))).thenReturn(response);
+
+        var requestBuilder = post("/product")
+                .contentType(APPLICATION_JSON)
+                .content(asJsonString(request));
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated());
+
+        verify(productController).create(any(CreateProductDTO.class));
+    }
 
     @Override
     CreateProductResource createController() {
